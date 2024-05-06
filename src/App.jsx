e638c05e2f0fcbef8d8768e736f3a809e3d6b77e
okashaoffice <okashaoffice75@gmail.com>
2024-05-06 05:08:14 -0700

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import Nav from "./components/Navbar/Nav";
 import Mainbody from "./components/Main/Mainbody";
 import Loginpage from "./components/Login/Login";
-import Totalbalance from "./components/Dashboard/Totalbalance";
-import Profit from "./components/Dashboard/Profit";
-import Collection from "./components/Dashboard/Collection";
-import { GrTransaction } from "react-icons/gr";
-import Transaction from "./components/Dashboard/Transaction";
 import Buycredit from "./components/BuyCredit/Buycredit";
 import Loginpass from "./components/Login/Loginpass";
-import Verifyemail from "./components/Login/Verifyemail";
-import Verifyemailbtn from "./components/Login/Verifyemailbtn";
-import NFTPL from "./components/Dashboard/NFTPL";
 import Dash from "./components/Dashboard/Dash";
 import Call from "./components/Login/Call";
 import ConnectWalllet from "./components/BuyCredit/ConnectWalllet";
-import WelcomPopup from "./components/Main/WelcomPopup";
-import StartTracking from "./components/Main/StartTracking";
 import { Route, Routes } from "react-router-dom";
 import OrderSummary from "./components/BuyCredit/OrderSummary";
 function App() {
